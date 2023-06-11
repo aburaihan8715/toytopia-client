14557d185ea08ce1c37498c0f97a63ee525ed3ad
aburahan8721 <aburaihan8721@gmail.com>
2023-06-11 11:07:54 +0600

    some correction

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,70 +1,44 @@
 const Blogs = () => {
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ullam magni voluptatum laborum sed ea? Eos quo vero accusantium, possimus
-        porro blanditiis numquam voluptatibus atque cupiditate distinctio dolore voluptatum perferendis.
-      </p>
+    <div className="py-8">
+      <div className="container mx-auto">
+        <div className="text-center mb-8">
+          <h2 className="text-3xl inline-block underline underline-offset-8 uppercase font-semibold text-secondary">our blogs</h2>
+        </div>
+        <div className="space-y-4 max-w-4xl mx-auto">
+          <div>
+            <h3 className="text-2xl font-medium">What is an access token and refresh token? How does it work and should store them on the client?</h3>
+            <p>
+              <strong>Ans : </strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus corporis quaerat quae qui voluptas ad
+              quidem, facere, illum eos doloribus temporibus nam dolores id praesentium reprehenderit maiores! Mollitia, delectus quae?
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-2xl font-medium">Compare SQL and NoSQL database?</h3>
+            <p>
+              <strong>Ans : </strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus corporis quaerat quae qui voluptas ad
+              quidem, facere, illum eos doloribus temporibus nam dolores id praesentium reprehenderit maiores! Mollitia, delectus quae?
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-2xl font-medium">What is express js? what is next js?</h3>
+            <p>
+              <strong>Ans : </strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus corporis quaerat quae qui voluptas ad
+              quidem, facere, illum eos doloribus temporibus nam dolores id praesentium reprehenderit maiores! Mollitia, delectus quae?
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-2xl font-medium">What is mongodb aggregate and how does it work?</h3>
+            <p>
+              <strong>Ans : </strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus corporis quaerat quae qui voluptas ad
+              quidem, facere, illum eos doloribus temporibus nam dolores id praesentium reprehenderit maiores! Mollitia, delectus quae?
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
